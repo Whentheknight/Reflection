b8afce26b0eba3ea15620ac30609855d9b55ab2d
Whentheknight <66561640+Whentheknight@users.noreply.github.com>
2021-09-06 16:55:49 +0200

    Add already existing solutions to repository

---
+++ AssemblyDemo/AssemblyDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.dll";
+
+            Assembly assembly = Assembly.LoadFile(path);
+            ShowAssemblyInfo(assembly);
+
+
+            Assembly ourAssembly = Assembly.GetExecutingAssembly();
+            ShowAssemblyInfo(ourAssembly);
+
+            Console.Read();
+        }
+
+        static void ShowAssemblyInfo(Assembly assembly)
+        {
+            Console.WriteLine(assembly.FullName);
+            Console.WriteLine("From GAC? {0}", assembly.GlobalAssemblyCache);
+            Console.WriteLine("Path: {0}", assembly.Location);
+            Console.WriteLine("Version: {0}", assembly.ImageRuntimeVersion);
+
+
+            foreach (Module method in assembly.GetModules())
+            {
+                Console.WriteLine(" Mod: {0}", method.Name);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AssemblyDemo2/AssemblyDemo2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.ServiceProcess.dll";
+
+            BindingFlags flags = BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance;
+
+            Assembly theAssembly = Assembly.LoadFrom(path);
+            Console.WriteLine(theAssembly.FullName);
+            Type[] types = theAssembly.GetTypes();
+
+            foreach (Type t in types)
+            {
+                Console.WriteLine(" Type: {0}", t.Name);
+                MemberInfo[] members = t.GetMembers(flags);
+
+                foreach (MemberInfo member in members)
+                {
+                    Console.WriteLine(" {0}: {1}", member.MemberType, member.Name);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
